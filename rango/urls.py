c9b6500df495202815e66008d5b2c0fd10e19f27
Luke Smith <lukeosmith@live.co.uk>
2023-01-30 21:11:11 +0000

    TWD2 Chapter 6 Done

--- rango/urls.py
+++ rango/urls.py
@@ -3,7 +3,9 @@ from rango import views
 
 app_name = 'rango'
 
-urlpatterns= [
+urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
+
